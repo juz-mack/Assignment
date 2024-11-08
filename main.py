515204f84b302b6ef9496c534ab0907c091fbfbe
juz-mack <justine.mackie28@gmail.com>
2024-11-08 14:57:50 +1100

    Working on functions

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ import dice
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
 dice_type = "Coin"
-
+#x = dice_type
 
 def select_face_option(event): #used when you select dice type
     global dice_type  # use global var named dice_type
-    ...  # replace with your own code
-    #dice.roll_dice()
+    
+    x = document.getElementbyId("dice").value 
+    #document.getElementbyId("dice").value - store as a variable and then store that value in dice_type
+    #run if function
+    #if(dice_type = 2)
 
 
 def roll_all_dice(event): #called when you press the sbmit button
     global dice_type  # use global var named dice_type
-    document.querySelector("div#roll-history").innerHTML = "Test"
+
+#add function to figure out output
+
+    #output = "???"
+    #document.querySelector("div#roll-history").innerHTML = output
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
