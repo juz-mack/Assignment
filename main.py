c3f1170c51741f702c5444cd9c890fb9a0b984cc
juz-mack <justine.mackie28@gmail.com>
2024-11-14 14:53:58 +1100

    Adding functions

--- main.py
+++ main.py
@@ -7,25 +7,39 @@ import dice
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
 dice_type = "Coin"
-
+#roll_dice(sides) = dice_num
 
 def select_face_option(event): #used when you select dice type
     global dice_type  # use global var named dice_type
+    #global roll_dice(sides)
 
     dice_type = document.getElementById("dice").value
-    print(dice_type)
+    if dice_type == "Coin":
+        dice_num = 2
+    elif dice_type == "d6":
+        dice_num = 6
+    elif dice_type == "d8":
+        dice_num = 8
+    elif dice_type == "d10":
+        dice_num = 10
+    elif dice_type == "d20":
+        dice_num = 20
+    elif dice_type == "d100":
+        dice_num = 100
 
-    #x = document.getElementbyId("dice").value 
-    #document.getElementbyId("dice").value - store as a variable and then store that value in dice_type
-    #run if function
-    #if dice_type =
+    roll_num = document.getElementById("num_roll").innerHTML
 
+    #run "dice"
 
 
 def roll_all_dice(event): #called when you press the submit button
     global dice_type  # use global var named dice_type
+    #get roll count from input box
+    ...
 
 #add function to figure out output
+
+    #for function?
 
     #output = "???"
     #document.querySelector("div#roll-history").innerHTML = output
