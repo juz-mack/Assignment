bb5b61c5855b331c67a8de784bbab07522a1a35e
juz-mack <justine.mackie28@gmail.com>
2024-11-15 15:02:22 +1100

    Adding functions print test

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import dice
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
 dice_type = "Coin"
+roll_num = "document.getElementById("num_roll").innerHTML"
 #roll_dice(sides) = dice_num
 
 def select_face_option(event): #used when you select dice type
     global dice_type  # use global var named dice_type
     #global roll_dice(sides)
+    global roll_num
 
     dice_type = document.getElementById("dice").value
     if dice_type == "Coin":
@@ -34,15 +36,19 @@ def select_face_option(event): #used when you select dice type
 
 def roll_all_dice(event): #called when you press the submit button
     global dice_type  # use global var named dice_type
+    global roll_num
     #get roll count from input box
     ...
 
 #add function to figure out output
 
+
     #for function?
 
-    #output = "???"
-    #document.querySelector("div#roll-history").innerHTML = output
+    output = "num"
+    document.querySelector("div#roll-history").innerHTML = output
+
+    print(output)
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
