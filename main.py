c4c2d60f74d63a8c4dc3f366a8584d5f19d6c313
juz-mack <justine.mackie28@gmail.com>
2024-11-22 12:31:44 +1100

    Updated for function in roll_all_dice

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ def roll_all_dice(event): #called when you press the submit button
     num_roll = document.getElementById("num_roll").value
     output = "Here are your rolls: "
 
-    for roll in range(dice_type, num_roll):
+    for _ in range(num_roll):
     #for loop goes here V dice roll and output needs to be collated
         roll_result = dice.roll_dice(dice_type)
         output = output + str(roll_result) + ", "
+        print(output)
 
-        #print(output)
     document.querySelector("div#roll-history").innerHTML = output
 
 def clear_history(event):
